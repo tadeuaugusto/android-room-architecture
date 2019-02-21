26915948281b8c00721eff99fa621b5275b30c82
tadeuaugusto <tadeuaugusto@gmail.com>
2019-02-21 12:24:24 -0300

    Adapter, ViewHolder and RecyclerView

--- app/src/main/java/com/example/roomarchitectureexample/MainActivity.java
+++ app/src/main/java/com/example/roomarchitectureexample/MainActivity.java
@@ -6,6 +6,8 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -19,13 +21,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         viewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         viewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(@Nullable List<Note> notes) {
 
-                // update RecycleView
-                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
+
             }
         });
     }
